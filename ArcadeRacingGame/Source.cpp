6fad57ab01443a17f5f6606eedf6fcfb03baa99d
yaelh <elhaeey@gmail.com>
2022-08-13 17:11:24 +0100

    drawing lines on screen to create track

--- ArcadeRacingGame/Source.cpp
+++ ArcadeRacingGame/Source.cpp
@@ -1,25 +1,78 @@
 
 #include "SFML/Graphics.hpp"
+#include <vector>
 
-int screen_w = 1000;
-int screen_h = 700;
+const int screen_w = 1000;
+int screen_h = 600;
+float road_w = screen_w * 0.8f;
+float tile_w = road_w * 0.1f;
+float depth = 1;
 
-
-void DrawPlane(sf::RenderWindow& window, sf::Color col, sf::Vector2f origin, float width, float height)
+struct Line
 {
-	sf::ConvexShape shape(3);
-	shape.setFillColor(col);
-
-	shape.setPointCount(4);
-	shape.setPoint(0, sf::Vector2f(0,screen_h));
-	shape.setPoint(1, sf::Vector2f(0, screen_h/2));
-	shape.setPoint(2, sf::Vector2f(screen_w, screen_h/2));
-	shape.setPoint(3, sf::Vector2f(screen_w, screen_h));
-
-	window.draw(shape);
+	sf::Vertex vertices[10];
+	Line() {};
 	
 
-}
+};
+
+
+void drawLines(sf::RenderWindow& window)
+{ 
+	
+	std::vector<Line> lines;
+
+	sf::Color grassDark(55, 154, 84);
+	sf::Color road(178, 195, 183);
+
+	for (int i = 0; i < screen_h / 2; i++)
+	{
+		Line line;
+		float y = (screen_h - i) / screen_h / 2;
+		line.vertices[0] = sf::Vertex(sf::Vector2f(0, screen_h - i), sf::Color::Green);
+		line.vertices[1] = sf::Vertex(sf::Vector2f(screen_w-road_w, (screen_h - i) / screen_h / 2), sf::Color::Green);
+
+		line.vertices[2] = sf::Vertex(sf::Vector2f(screen_w - road_w, screen_h-i), road);
+		line.vertices[3] = sf::Vertex(sf::Vector2f(road_w, screen_h-i), road);
+
+		line.vertices[4] = sf::Vertex(sf::Vector2f(road_w, screen_h - i), sf::Color::Green);
+		line.vertices[5] = sf::Vertex(sf::Vector2f(screen_w, screen_h - i), sf::Color::Green);
+
+
+		lines.push_back(line);
+		
+	}
+
+	/*
+	sf::Vertex vertices[10] =
+	{
+		sf::Vertex(sf::Vector2f(0,screen_h - 50),sf::Color::Green),
+		sf::Vertex(sf::Vector2f(screen_w-roadLStart,screen_h - 50),sf::Color::Green),
+
+		sf::Vertex(sf::Vector2f(screen_w-roadLStart,screen_h-50),sf::Color::White),
+		sf::Vertex(sf::Vector2f(roadLStart,screen_h-50),sf::Color::White),
+
+		sf::Vertex(sf::Vector2f(roadLStart,screen_h-50),sf::Color::Green),
+		sf::Vertex(sf::Vector2f(screen_w,screen_h-50),sf::Color::Green),
+
+	};
+	*/
+
+	for (int i = 0; i < lines.size(); i++)
+	{
+		window.draw(lines.at(i).vertices, 2, sf::Lines);
+	}
+
+
+	//window.draw(vertices,6,sf::Lines);
+
+};
+
+
+
+
+
+
 
 
 int main()
@@ -35,7 +88,8 @@ int main()
 				w.close();
 		}
 		w.clear();
-		DrawPlane(w, sf::Color::Green, sf::Vector2f(screen_w/2,screen_h/2),10,10);
+		
+		drawLines(w);
 		w.display();
 			
 	}
---
+++ ArcadeRacingGame/Vector3.h
@@ -0,0 +1,9 @@
+#ifndef _VECTOR3_H_
+#define _VECTOR3_H_
+
+namespace GameUtils
+{
+	
+}
+
+#endif
