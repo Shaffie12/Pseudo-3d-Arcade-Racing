9a0be8940a6f8863d81d39e4d115b93483bf1dbe
yaelh <elhaeey@gmail.com>
2023-08-12 03:06:48 +0100

    clean

--- ArcadeRacingGame/Segment.cpp
+++
@@ -1,8 +0,0 @@
-#include "Segment.h"
-
-Segment::Segment(int id,float curve, float dist) : screen_y(GameGlobals::GAME_H / 2)
-{
-	this->id = id;
-	curvature = curve;
-	length = dist;
-}
--- ArcadeRacingGame/TrackData.cpp
+++
@@ -1,9 +0,0 @@
-#include "TrackData.h"
-
-void TrackData::calculateTotalLength()
-{
-    for (Segment s : segments)
-        totalTrackLength += s.length;
-}
-
-
